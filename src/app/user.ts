f9429563c0aa04563670950dcb515a1a6821d295
Damodharan S <damodharan.shanmugamoorthyi@lighthousesolutions.tech>
2019-02-01 19:48:58 +0530

    Basic Angular TDF Form

---
+++ src/app/user.ts
@@ -0,0 +1,11 @@
+export class User {
+    constructor(
+        public name: string,
+        public age: number,
+        public email: string,
+        public phone: number,
+        public department: string,
+        public modeofEductaion: string,
+        public subscribe: boolean
+    ){}
+}
